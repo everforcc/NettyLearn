981520d695321a61f096ba21d257f042005104fe
everforcc <48779058+everforcc@users.noreply.github.com>
2022-12-20 15:47:48 +0800

    20221220 优化

--- src/test/java/cn/cc/netty/optimize/Test5ByteBuf.java
+++ src/test/java/cn/cc/netty/optimize/Test5ByteBuf.java
@@ -1,7 +1,6 @@
 package cn.cc.netty.optimize;
 
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.ChannelInitializer;
@@ -12,17 +11,21 @@ import io.netty.handler.logging.LoggingHandler;
 import lombok.extern.slf4j.Slf4j;
 
 /**
- * 5.
+ * 5. 测试
+ * ALLOCATOR
+ * RCVBUF_ALLOCATOR
  */
 @Slf4j
 public class Test5ByteBuf {
     public static void main(String[] args) {
 
         // 不池化
-        System.setProperty("io.netty.allocator.type","unpooled");
+        System.setProperty("io.netty.allocator.type", "unpooled");
         // 不首选直接内存，就是堆内存
-        System.setProperty("io.netty.noPreferDirect","true");
+        System.setProperty("io.netty.noPreferDirect", "true");
         // UnpooledByteBufAllocator$InstrumentedUnpooledUnsafeHeapByteBuf
+        // 7. io 直接内存io效率高，强制使用 Direct
+        // UnpooledByteBufAllocator$InstrumentedUnpooledUnsafeNoCleanerDirectByteBuf
 
         new ServerBootstrap()
                 .group(new NioEventLoopGroup())
@@ -34,11 +37,17 @@ public class Test5ByteBuf {
                         ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {
                             @Override
                             public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-                                ByteBuf buf = ctx.alloc().buffer();
-                                log.debug("alloc buf {}", buf);
+                                // 6.  ALLOCATOR
+                                // UnpooledByteBufAllocator$InstrumentedUnpooledUnsafeHeapByteBuf
+                                //ByteBuf buf = ctx.alloc().buffer();
+                                //log.debug("alloc buf {}", buf);
 
-//                                log.debug("receive buf {}", msg);
-//                                System.out.println("");
+                                // 7. RCVBUF_ALLOCATOR
+                                // UnpooledByteBufAllocator$InstrumentedUnpooledUnsafeNoCleanerDirectByteBuf
+                                // 最开始写上的原始数据
+                                // handler 是消费者
+                                log.debug("receive buf {}", msg);
+                                System.out.println("");
                             }
                         });
                     }
