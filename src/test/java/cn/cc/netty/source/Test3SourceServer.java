f08833f0765fec14c12ab33fb6818f8a498e88e7
everforcc <48779058+everforcc@users.noreply.github.com>
2023-04-14 15:59:39 +0800

    20230414 源码分析

---
+++ src/test/java/cn/cc/netty/source/Test3SourceServer.java
@@ -0,0 +1,26 @@
+package cn.cc.netty.source;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.logging.LoggingHandler;
+
+/**
+ * 分析源码
+ */
+public class Test3SourceServer {
+    public static void main(String[] args) {
+        new ServerBootstrap()
+                .group(new NioEventLoopGroup())
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel ch) {
+                        ch.pipeline().addLast(new LoggingHandler());
+                    }
+                }).bind(8080);
+    }
+}
