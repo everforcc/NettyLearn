369c91b804f9ee32f89dcdfa37d12ab6807080f6
everforcc <718497737@qq.com>
2025-12-25 21:16:17 +0800

    ChannelInboundHandlerAdapter使用示例

--- src/main/java/cn/cc/server/handler/QuitHandler.java
+++ src/main/java/cn/cc/server/handler/QuitHandler.java
@@ -4,17 +4,45 @@ import cn.cc.server.session.SessionFactory;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.timeout.IdleState;
+import io.netty.handler.timeout.IdleStateEvent;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @ChannelHandler.Sharable
 public class QuitHandler extends ChannelInboundHandlerAdapter {
 
-    // 当连接断开时触发 inactive 事件
+
+
+    /**
+     * Inactive 不活动的，不活跃的；
+     * 当连接断开时触发 inactive 事件
+     * @param ctx
+     * @throws Exception
+     */
     @Override
     public void channelInactive(ChannelHandlerContext ctx) throws Exception {
         SessionFactory.getSession().unbind(ctx.channel());
         log.debug("{} 已经断开", ctx.channel());
+    }
+
+    /**
+     *
+     * @param ctx
+     * @param evt
+     * @throws Exception
+     */
+    @Override
+    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+        super.userEventTriggered(ctx, evt);
+        if (evt instanceof IdleStateEvent){
+            IdleState state = ((IdleStateEvent) evt).state();
+            if (state == IdleState.READER_IDLE){
+                log.info("长时间未连接发送数据,中断...");
+                ctx.channel().close();
+            }
+        }
+        log.debug("{} 触发了事件 {}", ctx.channel(), evt);
     }
 
     // 当出现异常时触发
